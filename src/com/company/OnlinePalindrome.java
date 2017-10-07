5794e77610e5db7c009915b010cefc80ab5732da
Dzokaredevil <dima.voytovich@mail.ru>
2017-10-07 19:41:30 +0300

    task 9 completed

--- src/com/company/OnlinePalindrome.java
+++ src/com/company/OnlinePalindrome.java
@@ -5,7 +5,7 @@ package com.company;
  * Don't copy my code. The truth must be taken.
  */
 class OnlinePalindrome {
-    protected static boolean isPalindrome(char[] letters, int endIdx){
+    private static boolean isPalindrome(char[] letters, int endIdx){
         int left = 0, right = endIdx;
         while(left < right){
             if(letters[left] != letters[right])
@@ -16,11 +16,11 @@ class OnlinePalindrome {
         return true;
     }
 
-    static String checkPalindrome(String inStr, int base, int quotient){
+    static void checkPalindrome(String inStr){
         StringBuilder builder = new StringBuilder();
         char[] letters = inStr.toCharArray();
-        int leftHash   = letters[0] % quotient;
-        int rightHash  = letters[1] % quotient;
+        int leftHash   = letters[0] % 101;
+        int rightHash  = letters[1] % 101;
         int strLen     = inStr.length();
         builder.append("Y");
         for(int i = 1, h = 1; i < strLen; i++){
@@ -34,16 +34,16 @@ class OnlinePalindrome {
                     builder.append("N");
             }
             if(i == strLen-1)
-                break ;
+                break;
             if(i % 2 == 1){
-                rightHash = (rightHash + quotient - letters[(i+1)/2]*h);
-                rightHash = ((rightHash * base) % quotient + letters[i+1]) % quotient;
+                rightHash = (rightHash + 101 - letters[(i+1)/2]*h);
+                rightHash = ((rightHash * 256) % 101 + letters[i+1]) % 101;
             } else {
-                h = (h*base) % quotient;
-                leftHash  = (letters[i/2] * h + leftHash) % quotient;
-                rightHash = ((rightHash * base) % quotient + letters[i+1]) % quotient;
+                h = (h* 256) % 101;
+                leftHash  = (letters[i/2] * h + leftHash) % 101;
+                rightHash = ((rightHash * 256) % 101 + letters[i+1]) % 101;
             }
         }
-        return builder.toString();
+        builder.toString();
     }
 }
--- src/com/company/StreamMathRandom.java
+++ src/com/company/StreamMathRandom.java
@@ -6,8 +6,8 @@ import java.util.stream.Stream;
  * Created by dimav on 07.10.2017.
  * Don't copy my code. The truth must be taken.
  */
-public class StreamMathRandom {
-    public static Stream<Long> getRS(long a, long c, long m) {
-        return Stream.iterate(1L, n -> (a * n + c) % m);
+class StreamMathRandom {
+    static Stream<Long> getRS(long a, long c, long m){
+        return Stream.iterate(1L, n -> (a*n + c)%m);
     }
 }
